56e7e77addbcbcf4d85d778ee035182933d5d88e
Jack Engqvist <jacket@axis.com>
2015-10-13 14:31:52 +0200

    Updated documentation in code

--- src/nmea/nmea.h
+++ src/nmea/nmea.h
@@ -6,7 +6,11 @@
 #include <string.h>
 
 /* NMEA sentence types */
-typedef enum {NMEA_UNKNOWN, NMEA_GPGGA, NMEA_GPGLL} nmea_t;
+typedef enum {
+  NMEA_UNKNOWN,
+  NMEA_GPGGA,
+  NMEA_GPGLL
+} nmea_t;
 
 /* NMEA cardinal direction types */
 typedef char nmea_cardinal_t;
@@ -16,7 +20,11 @@ typedef char nmea_cardinal_t;
 #define NMEA_CARDINAL_DIR_WEST		(nmea_cardinal_t) 'W'
 #define NMEA_CARDINAL_DIR_UNKNOWN	(nmea_cardinal_t) '\0'
 
-/* NMEA data base struct */
+/**
+ * NMEA data base struct
+ *
+ * This struct will be extended by the parser data structs (ex: nmea_gpgll_s).
+ */
 typedef struct {
 	nmea_t type;
 	int errors;
@@ -29,18 +37,18 @@ typedef struct {
 	nmea_cardinal_t cardinal;
 } nmea_position;
 
-/* NMEA sentence max length */
-#define NMEA_MAX_LENGTH		50
+/* NMEA sentence max length (chars) */
+#define NMEA_MAX_LENGTH		255
 
-/* NMEA sentence endings */
+/* NMEA sentence endings, should be \r\n according the NMEA 0183 standard */
 #define NMEA_END_CHAR_1		'\n'
 #define NMEA_END_CHAR_2		'\n'
 
-/* NMEA sentence prefix length */
+/* NMEA sentence prefix length (chars) */
 #define NMEA_PREFIX_LENGTH	5
 
 /**
- * Get the type of the sentence.
+ * Get the sentence type.
  *
  * sentence needs to be a validated NMEA sentence string.
  *
@@ -58,10 +66,10 @@ extern nmea_t nmea_get_type(const char *sentence);
 extern uint8_t nmea_get_checksum(const char *sentence);
 
 /**
- * Check if the sentence has a precalculated checksum.
+ * Check if the sentence contains a precalculated checksum.
  *
  * sentence needs to be a validated NMEA sentence string.
- * length is the byte length of the sentence string.
+ * length is the character length of the sentence string.
  *
  * Return 0 if checksum exists, otherwise -1.
  */
@@ -70,25 +78,26 @@ extern int nmea_has_checksum(const char *sentence, int length);
 /**
  * Validate the sentence according to NMEA 0183.
  *
- * - Should start with a dollar sign.
- * - The next five chars should be uppercase letters.
- * - If it has a checksum, it checks it.
- * - Ends with \r\n (<CR><LF>).
+ * Criterias:
+ *   - Should start with a dollar sign.
+ *   - The next five characters should be uppercase letters.
+ *   - If it has a checksum, check it.
+ *   - Ends with the correct 2 characters.
  *
- * length is the byte length of the sentence string.
+ * length is the character length of the sentence string.
  *
  * Returns 0 if sentence is valid, otherwise -1.
  */
 extern int nmea_validate(const char *sentence, int length, int check_checksum);
 
 /**
- * Parses an NMEA sentence string.
+ * Parse an NMEA sentence string to a struct.
  *
  * sentence needs to be a validated NMEA sentence string.
- * length is the byte length of the sentence string.
+ * length is the character length of the sentence string.
  * check_checksum, if 1 and there is a checksum, validate it.
  *
- * Returns a pointer to a nmea data struct, or (nmea_s *) NULL if an error occurs.
+ * Returns a pointer to an NMEA data struct, or (nmea_s *) NULL if an error occurs.
  */
 extern nmea_s *nmea_parse(char *sentence, int length, int check_checksum);
 
