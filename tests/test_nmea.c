69b2439582de838dd948105557a675f2c23a9e54
Jack Engqvist <jacket@axis.com>
2015-10-22 16:30:52 +0200

    Prettier test result output

--- tests/test_nmea.c
+++ tests/test_nmea.c
@@ -15,11 +15,11 @@ test_get_type_ok()
 
   sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A,*1D\n\n");
   res = nmea_get_type(sentence);
-  mu_assert("nmea_get_type() should return correct type (GPGLL)", NMEA_GPGLL == res);
+  mu_assert("should return correct type (GPGLL)", NMEA_GPGLL == res);
 
 	sentence = strdup("$GPGGA,4916.45,N,12311.12,W,225444,A\n\n");
   res = nmea_get_type(sentence);
-  mu_assert("nmea_get_type() should return correct type (GPGGA)", NMEA_GPGGA == res);
+  mu_assert("should return correct type (GPGGA)", NMEA_GPGGA == res);
 
   return 0;
 }
@@ -32,15 +32,15 @@ test_get_type_unknown()
 
   sentence = strdup("THISISWRONG");
   res = nmea_get_type(sentence);
-  mu_assert("nmea_get_type() should return NMEA_UNKNOWN on unknown sentence type", NMEA_UNKNOWN == res);
+  mu_assert("should return NMEA_UNKNOWN on unknown sentence type", NMEA_UNKNOWN == res);
 
   sentence = strdup("$UNKNOWN");
   res = nmea_get_type(sentence);
-  mu_assert("nmea_get_type() should return NMEA_UNKNOWN on unknown sentence type", NMEA_UNKNOWN == res);
+  mu_assert("should return NMEA_UNKNOWN on unknown sentence type", NMEA_UNKNOWN == res);
 
   sentence = strdup("");
   res = nmea_get_type(sentence);
-  mu_assert("nmea_get_type() should return nmea_unknown on empty sentence", NMEA_UNKNOWN == res);
+  mu_assert("should return nmea_unknown on empty sentence", NMEA_UNKNOWN == res);
 
   return 0;
 }
@@ -51,7 +51,7 @@ test_get_checksum_with_crc()
 	// Sentence with checksum (0x1D == 29)
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A,*1D\n\n");
   uint8_t res = nmea_get_checksum(sentence);
-  mu_assert("nmea_get_checksum() should return correct checksum", 29 == res);
+  mu_assert("should return correct checksum", 29 == res);
 
   return 0;
 }
@@ -62,7 +62,7 @@ test_get_checksum_without_crc()
 	// Sentence without checksum (0x1D == 29)
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A,\n\n");
   uint8_t res = nmea_get_checksum(sentence);
-  mu_assert("nmea_get_checksum() should return correct checksum", 29 == res);
+  mu_assert("should return correct checksum", 29 == res);
 
   return 0;
 }
@@ -73,7 +73,7 @@ test_get_checksum_too_long_sentence()
 	// Sentence without correct ending (ex: \r\n)
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A,");
   uint8_t res = nmea_get_checksum(sentence);
-  mu_assert("nmea_get_checksum() should return 0 when sentence is too long", 0 == res);
+  mu_assert("should return 0 when sentence is too long", 0 == res);
 
   return 0;
 }
@@ -84,7 +84,7 @@ test_has_checksum_yes()
 	// Sentence with checksum
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A*1D\n\n");
   int res = nmea_has_checksum(sentence, strlen(sentence));
-  mu_assert("nmea_has_checksum() should return 0 when sentence has a checksum", 0 == res);
+  mu_assert("should return 0 when sentence has a checksum", 0 == res);
 
   return 0;
 }
@@ -95,7 +95,7 @@ test_has_checksum_no()
 	// Sentence without checksum
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A\n\n");
   int res = nmea_has_checksum(sentence, strlen(sentence));
-  mu_assert("nmea_has_checksum() should return -1 when sentence does not have a checksum", -1 == res);
+  mu_assert("should return -1 when sentence does not have a checksum", -1 == res);
 
   return 0;
 }
@@ -106,7 +106,7 @@ test_validate_ok_with_crc()
 	// Valid sentence with checksum
 	const char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A,*1D\n\n");
   int res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return 0 when sentence is valid", 0 == res);
+  mu_assert("should return 0 when sentence is valid", 0 == res);
 
   return 0;
 }
@@ -120,12 +120,12 @@ test_validate_ok_without_crc()
 	// Valid sentence without checksum
 	sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A\n\n");
   res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return 0 when sentence is valid", 0 == res);
+  mu_assert("should return 0 when sentence is valid", 0 == res);
 
 	// Valid sentence with invalid checksum
 	sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A*FF\n\n");
   res = nmea_validate(sentence, strlen(sentence), 0);
-  mu_assert("nmea_validate() should return 0 when check_checksum is 0 and crc is invalid", 0 == res);
+  mu_assert("should return 0 when check_checksum is 0 and crc is invalid", 0 == res);
 
   return 0;
 }
@@ -136,7 +136,7 @@ test_validate_fail_type()
 	// Invalid sentence type
 	const char *sentence = strdup("$GPgll,4916.45,N,12311.12,W,225444,A\n\n");
   int res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return -1 when sentence type is invalid", -1 == res);
+  mu_assert("should return -1 when sentence type is invalid", -1 == res);
 
   return 0;
 }
@@ -147,7 +147,7 @@ test_validate_fail_start()
 	// Invalid sentence start (no $ sign)
 	const char *sentence = strdup("£GPGLL,4916.45,N,12311.12,W,225444,A\n\n");
   int res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return -1 when sentence start is invalid", -1 == res);
+  mu_assert("should return -1 when sentence start is invalid", -1 == res);
 
   return 0;
 }
@@ -161,12 +161,12 @@ test_validate_fail_end()
 	// Invalid sentence ending (no \n)
 	sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A");
   res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return -1 when sentence ending is invalid", -1 == res);
+  mu_assert("should return -1 when sentence ending is invalid", -1 == res);
 
 	// Too short sentence
 	sentence = strdup("$");
   res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return -1 when sentence is too short", -1 == res);
+  mu_assert("should return -1 when sentence is too short", -1 == res);
 
   return 0;
 }
@@ -177,7 +177,7 @@ test_validate_fail_empty()
 	// Invalid sentence (empty string)
 	const char *sentence = strdup("");
   int res = nmea_validate(sentence, strlen(sentence), 1);
-  mu_assert("nmea_validate() should return -1 when sentence is empty", -1 == res);
+  mu_assert("should return -1 when sentence is empty", -1 == res);
 
   return 0;
 }
@@ -191,12 +191,12 @@ test_parse_ok()
   // With crc\n
   sentence = strdup("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47\n\n");
   res = nmea_parse(sentence, strlen(sentence), 1);
-  mu_assert("nmea_parse() should be able to parse a GPGGA sentence", NULL != res);
+  mu_assert("should be able to parse a GPGGA sentence", NULL != res);
 
   // With invalid crc, but check disabled
   sentence = strdup("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*FF\n\n");
   res = nmea_parse(sentence, strlen(sentence), 0);
-  mu_assert("nmea_parse() should be able to parse a GPGGA sentence", NULL != res);
+  mu_assert("should be able to parse a GPGGA sentence", NULL != res);
 
   return 0;
 }
@@ -209,7 +209,7 @@ test_parse_unknown()
 
   sentence = strdup("$JACK1,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47\n\n");
   res = nmea_parse(sentence, strlen(sentence), 1);
-  mu_assert("nmea_parse() should return NULL when sentence type is unknown", NULL == res);
+  mu_assert("should return NULL when sentence type is unknown", NULL == res);
 
   return 0;
 }
@@ -222,18 +222,18 @@ test_parse_invalid()
 
   sentence = strdup("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*FF\n\n");
   res = nmea_parse(sentence, strlen(sentence), 1);
-  mu_assert("nmea_parse() should return NULL when checksum is invalid", NULL != res);
+  mu_assert("should return NULL when checksum is invalid", NULL != res);
 
   sentence = strdup("");
   res = nmea_parse(sentence, strlen(sentence), 1);
-  mu_assert("nmea_parse() should return NULL when sentence is empty", NULL == res);
+  mu_assert("should return NULL when sentence is empty", NULL == res);
 
   sentence = strdup("invalid");
   res = nmea_parse(sentence, strlen(sentence), 1);
-  mu_assert("nmea_parse() should return NULL when sentence is invalid", NULL == res);
+  mu_assert("should return NULL when sentence is invalid", NULL == res);
 
   res = nmea_parse(NULL, 0, 1);
-  mu_assert("nmea_parse() should return NULL when sentence is NULL", NULL == res);
+  mu_assert("should return NULL when sentence is NULL", NULL == res);
 
   return 0;
 }
@@ -241,16 +241,20 @@ test_parse_invalid()
 static char *
 all_tests()
 {
+  mu_group("nmea_get_type()");
   mu_run_test(test_get_type_ok);
   mu_run_test(test_get_type_unknown);
 
+  mu_group("nmea_get_checksum()");
   mu_run_test(test_get_checksum_with_crc);
   mu_run_test(test_get_checksum_without_crc);
   mu_run_test(test_get_checksum_too_long_sentence);
 
+  mu_group("nmea_has_checksum()");
   mu_run_test(test_has_checksum_yes);
   mu_run_test(test_has_checksum_no);
 
+  mu_group("nmea_validate()");
   mu_run_test(test_validate_ok_with_crc);
   mu_run_test(test_validate_ok_without_crc);
 
@@ -259,6 +263,7 @@ all_tests()
   mu_run_test(test_validate_fail_end);
   mu_run_test(test_validate_fail_empty);
 
+  mu_group("nmea_parse()");
   mu_run_test(test_parse_ok);
   mu_run_test(test_parse_unknown);
   return 0;
@@ -267,13 +272,22 @@ all_tests()
 int
 main(void)
 {
+  tests_run = 0;
+
   char *result = all_tests();
   if (result != 0) {
-    printf("\n%s\n", result);
-  } else {
-    printf("\n\nALL TESTS PASSED\n");
+    printf(KERR "\n");
+    printf("╔═╗╔═╗╦╦  ╔═╗╔╦╗\n");
+    printf("╠╣ ╠═╣║║  ║╣  ║║\n");
+    printf("╚  ╩ ╩╩╩═╝╚═╝═╩╝\n" KNRM);
+    printf("All tests did not pass!\n\n");
+    exit(EXIT_FAILURE);
   }
-  printf("Tests run: %d\n", tests_run);
 
-  return result != 0;
+  printf(KGRN "\n");
+  printf("╔═╗╔═╗╔═╗╔═╗╔═╗╔╦╗\n");
+  printf("╠═╝╠═╣╚═╗╚═╗║╣  ║║\n");
+  printf("╩  ╩ ╩╚═╝╚═╝╚═╝═╩╝\n" KNRM);
+  printf("All %d tests passed!\n\n", tests_run);
+  exit(EXIT_SUCCESS);
 }
--- tests/test_parse.c
+++ tests/test_parse.c
@@ -17,30 +17,30 @@ test_position_parse_ok()
   /* 3 digit degrees */
   s = strdup("12311.12");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return 0 when successfull", 0 == res);
-  mu_assert("nmea_position_parse(), pos->degrees should be 123", 123 == pos->degrees);
-  mu_assert("nmea_position_parse(), pos->minutes should be 11.12", 11.12 == pos->minutes);
+  mu_assert("should return return 0 when successfull", 0 == res);
+  mu_assert("pos->degrees should be 123", 123 == pos->degrees);
+  mu_assert("pos->minutes should be 11.12", 11.12 == pos->minutes);
 
   /* 2 digit degrees */
   s = strdup("2621.44");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return 0 when successfull", 0 == res);
-  mu_assert("nmea_position_parse(), pos->degrees should be 123", 26 == pos->degrees);
-  mu_assert("nmea_position_parse(), pos->minutes should be 11.12", 21.44 == pos->minutes);
+  mu_assert("should return return 0 when successfull", 0 == res);
+  mu_assert("pos->degrees should be 123", 26 == pos->degrees);
+  mu_assert("pos->minutes should be 11.12", 21.44 == pos->minutes);
 
   /* 1 digit degrees */
   s = strdup("901.01");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return 0 when successfull", 0 == res);
-  mu_assert("nmea_position_parse(), pos->degrees should be 123", 9 == pos->degrees);
-  mu_assert("nmea_position_parse(), pos->minutes should be 11.12", 1.01 == pos->minutes);
+  mu_assert("should return return 0 when successfull", 0 == res);
+  mu_assert("pos->degrees should be 123", 9 == pos->degrees);
+  mu_assert("pos->minutes should be 11.12", 1.01 == pos->minutes);
 
   /* 0 decimals in minutes */
   s = strdup("901.");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return 0 when successfull", 0 == res);
-  mu_assert("nmea_position_parse(), pos->degrees should be 123", 9 == pos->degrees);
-  mu_assert("nmea_position_parse(), pos->minutes should be 11.12", 1.0 == pos->minutes);
+  mu_assert("should return return 0 when successfull", 0 == res);
+  mu_assert("pos->degrees should be 123", 9 == pos->degrees);
+  mu_assert("pos->minutes should be 11.12", 1.0 == pos->minutes);
 
   return 0;
 }
@@ -55,16 +55,16 @@ test_position_parse_fail()
   /* no minutes */
   s = strdup("123");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return -1 on failure", -1 == res);
+  mu_assert("should return return -1 on failure", -1 == res);
 
   /* empty string */
   s = strdup("");
   res = nmea_position_parse(s, pos);
-  mu_assert("nmea_position_parse() should return return -1 on empty string", -1 == res);
+  mu_assert("should return return -1 on empty string", -1 == res);
 
   /* NULL */
   res = nmea_position_parse(NULL, pos);
-  mu_assert("nmea_position_parse() should return return -1 on NULL", -1 == res);
+  mu_assert("should return return -1 on NULL", -1 == res);
 
   return 0;
 }
@@ -76,19 +76,19 @@ test_cardinal_dir_parse_ok()
 
   /* North */
   card = nmea_cardinal_direction_parse("N");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_NORTH on \"N\"", NMEA_CARDINAL_DIR_NORTH == card);
+  mu_assert("should return NMEA_CARDINAL_NORTH on \"N\"", NMEA_CARDINAL_DIR_NORTH == card);
 
   /* East */
   card = nmea_cardinal_direction_parse("E");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_EAST on \"E\"", NMEA_CARDINAL_DIR_EAST == card);
+  mu_assert("should return NMEA_CARDINAL_EAST on \"E\"", NMEA_CARDINAL_DIR_EAST == card);
 
   /* South */
   card = nmea_cardinal_direction_parse("S");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_SOUTH on \"S\"", NMEA_CARDINAL_DIR_SOUTH == card);
+  mu_assert("should return NMEA_CARDINAL_SOUTH on \"S\"", NMEA_CARDINAL_DIR_SOUTH == card);
 
   /* West */
   card = nmea_cardinal_direction_parse("W");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_WEST on \"W\"", NMEA_CARDINAL_DIR_WEST == card);
+  mu_assert("should return NMEA_CARDINAL_WEST on \"W\"", NMEA_CARDINAL_DIR_WEST == card);
 
   return 0;
 }
@@ -100,19 +100,19 @@ test_cardinal_dir_parse_fail()
 
   /* Empty string */
   card = nmea_cardinal_direction_parse("");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_UNKNOWN on empty string", NMEA_CARDINAL_DIR_UNKNOWN == card);
+  mu_assert("should return NMEA_CARDINAL_UNKNOWN on empty string", NMEA_CARDINAL_DIR_UNKNOWN == card);
 
   /* NULL */
   card = nmea_cardinal_direction_parse(NULL);
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_UNKNOWN on NULL", NMEA_CARDINAL_DIR_UNKNOWN == card);
+  mu_assert("should return NMEA_CARDINAL_UNKNOWN on NULL", NMEA_CARDINAL_DIR_UNKNOWN == card);
 
   /* Unknown character (J) */
   card = nmea_cardinal_direction_parse("J");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_UNKNOWN on \"J\"", NMEA_CARDINAL_DIR_UNKNOWN == card);
+  mu_assert("should return NMEA_CARDINAL_UNKNOWN on \"J\"", NMEA_CARDINAL_DIR_UNKNOWN == card);
 
   /* Unknown character (n) */
   card = nmea_cardinal_direction_parse("n");
-  mu_assert("nmea_cardinal_parse() should return NMEA_CARDINAL_UNKNOWN on \"n\"", NMEA_CARDINAL_DIR_UNKNOWN == card);
+  mu_assert("should return NMEA_CARDINAL_UNKNOWN on \"n\"", NMEA_CARDINAL_DIR_UNKNOWN == card);
 
   return 0;
 }
@@ -125,17 +125,17 @@ test_time_parse_ok()
 
   /* Parse time, no leading zeros */
   rv = nmea_time_parse("171521", &t);
-  mu_assert("nmea_time_parse() should return 0 on success", 0 == rv);
-  mu_assert("nmea_time_parse(), t.tm_sec should be 21", 21 == t.tm_sec);
-  mu_assert("nmea_time_parse(), t.tm_min should be 15", 15 == t.tm_min);
-  mu_assert("nmea_time_parse(), t.tm_hour should be 17", 17 == t.tm_hour);
+  mu_assert("should return 0 on success", 0 == rv);
+  mu_assert("t.tm_sec should be 21", 21 == t.tm_sec);
+  mu_assert("t.tm_min should be 15", 15 == t.tm_min);
+  mu_assert("t.tm_hour should be 17", 17 == t.tm_hour);
 
   /* Parse time, with leading zeros */
   rv = nmea_time_parse("090801", &t);
-  mu_assert("nmea_time_parse() should return 0 on success", 0 == rv);
-  mu_assert("nmea_time_parse(), t.tm_sec should be 1", 1 == t.tm_sec);
-  mu_assert("nmea_time_parse(), t.tm_min should be 8", 8 == t.tm_min);
-  mu_assert("nmea_time_parse(), t.tm_hour should be 9", 9 == t.tm_hour);
+  mu_assert("should return 0 on success", 0 == rv);
+  mu_assert("t.tm_sec should be 1", 1 == t.tm_sec);
+  mu_assert("t.tm_min should be 8", 8 == t.tm_min);
+  mu_assert("t.tm_hour should be 9", 9 == t.tm_hour);
 
   return 0;
 }
@@ -148,34 +148,34 @@ test_time_parse_fail()
 
   /* Invalid time, hours over 23 */
   rv = nmea_time_parse("240000", &t);
-  mu_assert("nmea_time_parse() should return -1 when hours is over 23", -1 == rv);
+  mu_assert("should return -1 when hours is over 23", -1 == rv);
 
   /* Invalid time, minutes over 59 */
   rv = nmea_time_parse("006000", &t);
-  mu_assert("nmea_time_parse() should return -1 when minutes is over 59", -1 == rv);
+  mu_assert("should return -1 when minutes is over 59", -1 == rv);
 
   /*
    * Invalid time, seconds over 61 (tm_sec is generally 0-59. The extra range is
    * to accommodate for leap seconds in certain systems).
    **/
   rv = nmea_time_parse("000062", &t);
-  mu_assert("nmea_time_parse() should return -1 when seconds is over 59", -1 == rv);
+  mu_assert("should return -1 when seconds is over 59", -1 == rv);
 
   /* Too short string */
   rv = nmea_time_parse("2345", &t);
-  mu_assert("nmea_time_parse() should return -1 on too short string", -1 == rv);
+  mu_assert("should return -1 on too short string", -1 == rv);
 
   /* Invalid characters */
   rv = nmea_time_parse("2345XX", &t);
-  mu_assert("nmea_time_parse() should return -1 on invalid characters", -1 == rv);
+  mu_assert("should return -1 on invalid characters", -1 == rv);
 
   /* Empty string */
   rv = nmea_time_parse("", &t);
-  mu_assert("nmea_time_parse() should return -1 on empty string", -1 == rv);
+  mu_assert("should return -1 on empty string", -1 == rv);
 
   /* NULL */
   rv = nmea_time_parse(NULL, &t);
-  mu_assert("nmea_time_parse() should return -1 on NULL", -1 == rv);
+  mu_assert("should return -1 on NULL", -1 == rv);
 
   return 0;
 }
@@ -183,12 +183,15 @@ test_time_parse_fail()
 static char *
 all_tests()
 {
+  mu_group("nmea_position_parse()");
   mu_run_test(test_position_parse_ok);
   mu_run_test(test_position_parse_fail);
 
+  mu_group("nmea_cardinal_dir_parse()");
   mu_run_test(test_cardinal_dir_parse_ok);
   mu_run_test(test_cardinal_dir_parse_fail);
 
+  mu_group("nmea_time_parse()");
   mu_run_test(test_time_parse_ok);
   mu_run_test(test_time_parse_fail);
 
@@ -198,13 +201,22 @@ all_tests()
 int
 main(void)
 {
+  tests_run = 0;
+
   char *result = all_tests();
   if (result != 0) {
-    printf("\n%s\n", result);
-  } else {
-    printf("\n\nALL TESTS PASSED\n");
+    printf(KERR "\n");
+    printf("╔═╗╔═╗╦╦  ╔═╗╔╦╗\n");
+    printf("╠╣ ╠═╣║║  ║╣  ║║\n");
+    printf("╚  ╩ ╩╩╩═╝╚═╝═╩╝\n" KNRM);
+    printf("All tests did not pass!\n\n");
+    exit(EXIT_FAILURE);
   }
-  printf("Tests run: %d\n", tests_run);
 
-  return result != 0;
+  printf(KGRN "\n");
+  printf("╔═╗╔═╗╔═╗╔═╗╔═╗╔╦╗\n");
+  printf("╠═╝╠═╣╚═╗╚═╗║╣  ║║\n");
+  printf("╩  ╩ ╩╚═╝╚═╝╚═╝═╩╝\n" KNRM);
+  printf("All %d tests passed!\n\n", tests_run);
+  exit(EXIT_SUCCESS);
 }
