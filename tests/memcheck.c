16fc7fc0dda97794eecba61a1dad6f4f8f0a9d6e
Jack Engqvist Johansson <jack@jack-ubuntu.(none)>
2015-10-29 21:24:04 +0100

    Added memcheck.c

---
+++ tests/memcheck.c
@@ -0,0 +1,83 @@
+// This file parses standard input and the exits.
+// Should be used together with valgrind to check for memory leaks.
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <nmea.h>
+#include <nmea/gpgll.h>
+#include <nmea/gpgga.h>
+
+int
+main(void)
+{
+	char *buffer;
+	int gps_fd;
+	int read_bytes, total_bytes = 0;
+	char *start, *end;
+	sigset_t block_mask;
+
+	buffer = malloc(4096);
+	if (NULL == buffer) {
+		perror("malloc buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	gps_fd = 0; // stdin
+
+	while (1) {
+		read_bytes = read(gps_fd, buffer + total_bytes, 20);
+		if (-1 == read_bytes) {
+			perror("read stdin");
+			exit(EXIT_FAILURE);
+		}
+		if (0 == read_bytes) {
+			break;
+ 		}
+
+		total_bytes += read_bytes;
+
+		/* find start (a dollar $ign) */
+		start = memchr(buffer, '$', total_bytes);
+		if (NULL == start) {
+			total_bytes = 0;
+			continue;
+		}
+
+		/* find end of line */
+		end = memchr(start, NMEA_END_CHAR_1, total_bytes - (start - buffer));
+		if (NULL == end || NMEA_END_CHAR_2 != *(++end)) {
+			continue;
+		}
+
+		/* handle data */
+		char buf[255];
+		nmea_s *data;
+
+		data = nmea_parse(start, end - start + 1, 0);
+		if (NULL != data) {
+			nmea_free(data);
+		}
+
+		/* buffer empty? */
+		if (end == buffer + total_bytes) {
+			total_bytes = 0;
+			continue;
+		}
+
+		/* copy rest of buffer to beginning */
+		if (buffer != memmove(buffer, end, total_bytes - (end - buffer))) {
+			total_bytes = 0;
+			continue;
+		}
+
+		total_bytes -= end - buffer;
+	}
+
+cleanup:
+	close(gps_fd);
+	free(buffer);
+	return 0;
+}
