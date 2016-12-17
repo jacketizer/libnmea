9f31b805f4f50bcbb2e987919446dea6c16d89ab
Jack Engqvist Johansson <jack@xtal.blue>
2016-12-17 21:31:16 +0100

    Changed parser array structure for static parsers

--- src/nmea/parser_static.c
+++ src/nmea/parser_static.c
@@ -12,8 +12,8 @@ nmea_parser_module_s parsers[3];
 	extern int gp##modname##_free_data(nmea_s *data); \
 	extern int gp##modname##_parse(nmea_parser_s *parser, char *value, int val_index);
 
-#define PARSER_LOAD(modname, modtype) \
-	parser = &(parsers[(int) modtype - 1]); \
+#define PARSER_LOAD(modname) \
+	parser = &(parsers[i]); \
 	parser->handle = NULL; \
 	parser->allocate_data = gp##modname##_allocate_data; \
 	parser->set_default = gp##modname##_set_default; \
@@ -22,6 +22,7 @@ nmea_parser_module_s parsers[3];
 	if (-1 == gp##modname##_init((nmea_parser_s *) parser)) { \
 		return -1; \
 	} \
+	i++;
 
 #define PARSER_COUNT (sizeof parsers / sizeof (parsers[0]))
 
@@ -39,11 +40,12 @@ nmea_init_parser(const char *filename)
 int
 nmea_load_parsers()
 {
+	int i = 0;
 	nmea_parser_module_s *parser;
 
-	PARSER_LOAD(gll, NMEA_GPGLL);
-	PARSER_LOAD(gga, NMEA_GPGGA);
-	PARSER_LOAD(rmc, NMEA_GPRMC);
+	PARSER_LOAD(gll);
+	PARSER_LOAD(gga);
+	PARSER_LOAD(rmc);
 
 	return PARSER_COUNT;
 }
@@ -58,22 +60,28 @@ nmea_unload_parsers()
 nmea_parser_module_s *
 nmea_get_parser_by_type(nmea_t type)
 {
-	return &(parsers[(int) type - 1]);
+	int i;
+
+	for (i = 0; i < PARSER_COUNT; i++) {
+		if (type == parsers[i].parser.type) {
+			return &(parsers[i]);
+		}
+	}
+
+	return (nmea_parser_module_s *) NULL;
 }
 
 nmea_parser_module_s *
 nmea_get_parser_by_sentence(const char *sentence)
 {
 	int i;
-	nmea_parser_s *p;
 
 	for (i = 0; i < PARSER_COUNT; i++) {
-		p = (nmea_parser_s *) (parsers + i);
-		if (NULL == p->type_word) {
+		if (NULL == parsers[i].parser.type_word) {
 			continue;
 		}
 
-		if (0 == strncmp(sentence + 1, p->type_word, NMEA_PREFIX_LENGTH)) {
+		if (0 == strncmp(sentence + 1, parsers[i].parser.type_word, NMEA_PREFIX_LENGTH)) {
 			return &(parsers[i]);
 		}
 	}
