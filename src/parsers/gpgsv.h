435f1780b9ca683d9dbfc00a4668594ecd3213e7
HarryManderTait <41089556+HarryManderTait@users.noreply.github.com>
2019-10-10 11:38:30 +1300

    Fix gpgsv struct extension offset (#37)
    The nmea_s base element was not added to the nmea_gpgsv_s struct,
    causing sentences and sentence_number to be overwritten in nmea_parse

--- src/parsers/gpgsv.h
+++ src/parsers/gpgsv.h
@@ -7,6 +7,7 @@
 #include <nmea.h>
 
 typedef struct {
+	nmea_s base;
 	unsigned int sentences; //Number of sentences for full data
 	unsigned int sentence_number; //Current sentence number
 	unsigned int satellites; //Number of satellites in view
